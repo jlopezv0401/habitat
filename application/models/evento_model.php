d1ed17e6724e13b16e0b9c7a07876756e595782f
Jesus Lopez <charrosnegrosborrachos@hotmail.com>
2012-06-18 01:24:50 -0500

    Ya bien

--- application/models/evento_model.php
+++ application/models/evento_model.php
@@ -4,7 +4,6 @@ class Evento_model extends CI_Model {
 	public function __construct(){
 		$this->load->database();
 	}
-<<<<<<< HEAD
 
     public function create_evento(){
         $this->load->helper('url');
@@ -40,51 +39,5 @@ class Evento_model extends CI_Model {
         );
 
         return $this->db->delete('Evento', $data);
-
     }
-=======
-	
-
-    public function create_evento(){
-        $this->load->helper('url');
-        $data= array(
-            'nombre' =>  $this->input->post('nombre'),
-            'ubicacion' =>  $this->input->post('ubicacion'),
-            'fecha_inicio' =>  $this->input->post('fecha_inicio'),
-            'fecha_fin' =>  $this->input->post('fecha_fin')
-        );
-
-        return $this->db->insert('Evento', $data);
-    }
-
-	public function read_evento(){
-		$eventos = $this->db->get('Evento');
-		return $eventos->result_array();
-	}
-
-    public function update_evento(){
-        $data= array(
-            'nombre' =>  $this->input->post('nombre'),
-            'ubicacion' =>  $this->input->post('ubicacion'),
-            'fecha_inicio' =>  $this->input->post('fecha_inicio'),
-            'fecha_fin' =>  $this->input->post('fecha_fin')
-        );
-        $this->db->where('id', $this->input->post('id'));
-        return $this->db->update('Evento', $data);
-    }
-
-    public function del_evento(){
-        $data= array(
-            'id' => $this->input->post('id')
-        );
-
-        return $this->db->delete('Evento', $data);
-
-    }
-	
-	public function show_evento(){
-		$eventos = $this->db->get('Evento');
-		return $eventos->result_array();
-	}
->>>>>>> f26fcce37ce2538279467184e1f5f0177ecbc92b
 }
